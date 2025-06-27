69386d3733486510703bbb170efa2927cbe53a63
cozycornerpatios-fabric <cozycornerpatios@gmail.com>
2025-06-27 17:11:08 +0530

    Update rect+trap.py

--- rect+trap.py
+++ rect+trap.py
@@ -13,7 +13,7 @@ os.makedirs(PDF_DIR, exist_ok=True)
 
 @app.route('/')
 def index():
-    return render_template_string(open("form.html").read())
+    return render_template_string(open("r+t.html").read())
 
 @app.route('/generate-cushions', methods=['POST'])
 def generate_cushions():
