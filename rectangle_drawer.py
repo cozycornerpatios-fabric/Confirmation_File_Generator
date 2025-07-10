2dc17910c218322d2565a5de7d2a19d910328248
cozycornerpatios-fabric <cozycornerpatios@gmail.com>
2025-07-10 20:00:10 +0530

    Update rectangle_drawer.py

--- rectangle_drawer.py
+++ rectangle_drawer.py
@@ -90,7 +90,7 @@ def draw_rectangle(c, cushion):
 
     ]
     left_x = 1 * inch
-    y = page_height - 3 * inch # Adjusted initial y position for specs
+    # y = page_height - 3 * inch # Adjusted initial y position for specs
     
 
     for label, value in specs:
