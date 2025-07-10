03dcf90702686eeb097f284ab4f1ad76729a064f
cozycornerpatios-fabric <cozycornerpatios@gmail.com>
2025-07-10 19:57:59 +0530

    Update right_triangle_drawer.py

--- right_triangle_drawer.py
+++ right_triangle_drawer.py
@@ -51,7 +51,7 @@ def draw_right_triangle(c, cushion):
         ("Fill",      cushion.get("fill", ""))
     ]
     left_x = 1 * inch
-    y = page_h - 3 * inch # Adjusted initial y position for specs
+    # y = page_h - 3 * inch # Adjusted initial y position for specs
     
 
     for label, value in specs:
