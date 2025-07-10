32c6fdc60d3dfe7b9bc9ab9c431b266e17d90562
cozycornerpatios-fabric <cozycornerpatios@gmail.com>
2025-07-10 20:01:35 +0530

    Update curved_drawer.py

--- curved_drawer.py
+++ curved_drawer.py
@@ -54,7 +54,7 @@ def draw_curved(c, cushion):
         ("Fill",         fill)
     ]
     left_x = 1 * inch
-    y = page_height - 3 * inch # Adjusted initial y position for specs
+    # y = page_height - 3 * inch # Adjusted initial y position for specs
     
 
     for label, value in specs:
